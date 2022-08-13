2d1b008c54e2a26ce53746ebf59870ac40221acc
Gaurav <gaurav.narayan.varma@gmail.com>
2022-08-13 16:06:15 -0400

    commit tree 0814812ce462e547742aa09e51f8a935e095d9db

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution here
+
+const drivers = ['Alixon', 'Umut', 'Ian']
+
+function findMatching(driverList, string){
+    return driverList.filter((name) => name.toLowerCase() === string.toLowerCase())
+}
+
+// function fuzzyMatch(driverList, string){
+//     // iterates through driverList
+//     return driverList.filter((name) => {
+//         // iterates through given string
+//         for (let i = 0; i < string.length; i++){
+//             if (name[i] === string[i]){
+//                 // if it matches it does nothing
+//             } else {
+//                 // if not, returns false
+//                 return false
+//             }
+//         }
+//         // getting out the of the for loop means that the letters matched
+//         // hence return true
+//         return true
+//     })
+// }
+
+// function fuzzyMatch(source, testString) {
+//     return source.filter(
+//       (possibleMatch) =>
+//         possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+//     );
+//   }
+
+function fuzzyMatch(driverList, string){
+    return driverList.filter((element) => {
+        return element.toLowerCase().indexOf(string.toLowerCase()) === 0
+    })
+}
+
+
+const driverObjects = [
+    {name: 'Ian', hometown: 'Vermont'},
+    {name: 'Umut', hometown: 'Turkey'}
+]
+
+function matchName(driverInfo, string){
+    return driverInfo.filter(element => element.name === string)
+}
